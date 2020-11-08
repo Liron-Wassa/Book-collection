8fb9f1d9c6ac9433c09efb0e696383f8ff25c41a
Liron Wassa <khruiuuxv@gmail.com>
2020-11-08 13:26:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Liron-Wassa/Book-collection/precache-manifest.f52628f222f8971983d450a1507ba7eb.js"
+  "//Book-collection/precache-manifest.a28b09aa62da03e81fb731564dfb826e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Liron-Wassa/Book-collection/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//Book-collection/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
